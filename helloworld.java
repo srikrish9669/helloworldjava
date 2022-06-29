5e51408f060d0b768ce51ca6d5c1cd718ae8af16
srikrish9669 <52035165+srikrish9669@users.noreply.github.com>
2022-06-29 11:46:44 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Edited by Srikrishna Vura on 29th June 11:26");
+    System.out.println("Edited by Srikrishna Vura and Enabled Webhook on 29th June 11:46");
     }
 }
