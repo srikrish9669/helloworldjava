ed1a592c08c8deaf6c0a6cef8d84c12dc975f9b8
srikrish9669 <52035165+srikrish9669@users.noreply.github.com>
2022-06-29 11:27:06 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 19th june triggered by webhook");
+    System.out.println("Edited by Srikrishna Vura on 29th June 11:26");
     }
 }
